35b03a3c2404b112b2fbe1b0fd5afd72bf459b97
Tinh <tinhdang6868@gmail.com>
2026-03-20 21:16:37 +0700

    Api chi tiết món ăn

--- FastFoodOrdering.Api/DTOs/Product/ProductDetailDto.cs
+++ FastFoodOrdering.Api/DTOs/Product/ProductDetailDto.cs
@@ -2,5 +2,9 @@
 
 public class ProductDetailDto
 {
-
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
 }
--- FastFoodOrdering.Api/Services/Interfaces/IProductService.cs
+++ FastFoodOrdering.Api/Services/Interfaces/IProductService.cs
@@ -6,4 +6,5 @@ namespace FastFoodOrdering.Api.Services.Interfaces;
 public interface IProductService
 {
     Task<IEnumerable<ProductListItemDto>> GetAllProductAsync();
+    Task<ProductDetailDto?> GetProductByIdAsync(int id);
 }
